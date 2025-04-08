33239f22bf6f0087a8c7ae494673f9e6df6c2aa6
wenjianzhang <zwj777@live.com>
2025-04-08 18:45:38 +0800

    refactor🎨: add format and output configuration fields to logger struct in logger.go

--- sdk/config/logger.go
+++ sdk/config/logger.go
@@ -9,6 +9,8 @@ type Logger struct {
 	Stdout    string
 	EnabledDB bool
 	Cap       uint
+	Format    string // 新增日志格式配置
+	Output    string // 新增日志输出目标配置
 }
 
 // Setup 设置logger
@@ -19,6 +21,8 @@ func (e Logger) Setup() {
 		logger.WithLevel(e.Level),
 		logger.WithStdout(e.Stdout),
 		logger.WithCap(e.Cap),
+		logger.WithFormat(e.Format), // 应用新的配置
+		logger.WithOutput(e.Output), // 应用新的配置
 	)
 }
 
