426758359a04bc65d956224bb34520d4180e01f6
lj <lujun151@aliyun.com>
2025-10-15 17:39:05 +0800

    feat✨:  Add containspath case条件

--- tools/search/query.go
+++ tools/search/query.go
@@ -80,6 +80,8 @@ func pgSql(driver string, t *resolveSearchTag, condition Condition, qValue refle
 		condition.SetWhere(fmt.Sprintf("%s.%s ilike ?", t.Table, t.Column), []interface{}{"%" + qValue.Field(i).String() + "%"})
 	case "contains":
 		condition.SetWhere(fmt.Sprintf("%s.%s like ?", t.Table, t.Column), []interface{}{"%" + qValue.Field(i).String() + "%"})
+	case "containspath":
+		condition.SetWhere(fmt.Sprintf("%s.%s like ?", t.Table, t.Column), []interface{}{"%/" + qValue.Field(i).String() + "/%"})
 	case "gt":
 		condition.SetWhere(fmt.Sprintf("%s.%s > ?", t.Table, t.Column), []interface{}{qValue.Field(i).Interface()})
 	case "gte":
@@ -127,6 +129,8 @@ func otherSql(driver string, t *resolveSearchTag, condition Condition, qValue re
 		condition.SetWhere(fmt.Sprintf("`%s`.`%s` = ?", t.Table, t.Column), []interface{}{qValue.Field(i).Interface()})
 	case "contains", "icontains":
 		condition.SetWhere(fmt.Sprintf("`%s`.`%s` like ?", t.Table, t.Column), []interface{}{"%" + qValue.Field(i).String() + "%"})
+	case "containspath":
+		condition.SetWhere(fmt.Sprintf("`%s`.`%s` like ?", t.Table, t.Column), []interface{}{"%/" + qValue.Field(i).String() + "/%"})
 	case "gt":
 		condition.SetWhere(fmt.Sprintf("`%s`.`%s` > ?", t.Table, t.Column), []interface{}{qValue.Field(i).Interface()})
 	case "gte":
