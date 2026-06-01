a61537a29ebf29207e9e794f897f8df071a22374
lj <lujun151@aliyun.com>
2026-06-01 10:24:42 +0800

    fix: 实现casbin Logger接口的LogError方法以兼容v2.77.2

--- sdk/pkg/casbin/log.go
+++ sdk/pkg/casbin/log.go
@@ -63,6 +63,14 @@ func (l *Logger) LogPolicy(policy map[string][][]string) {
 	logger.DefaultLogger.Fields(data).Log(logger.InfoLevel, nil)
 }
 
+// LogError log info related to error.
+func (l *Logger) LogError(err error, msg ...string) {
+	logger.DefaultLogger.Fields(map[string]interface{}{
+		"error": err,
+		"msg":   msg,
+	}).Log(logger.ErrorLevel, nil)
+}
+
 //func (l *Logger) Print(v ...interface{}) {
 //	if l.IsEnabled() {
 //		logger.DefaultLogger.Log(logger.InfoLevel, v...)
